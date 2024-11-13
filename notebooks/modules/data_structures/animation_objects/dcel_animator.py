b46ff25851c476e061a0b7ef1835d8fafe92f5cb
DasWHYYYDerWoche <139765359+DasWHYYYDerWoche@users.noreply.github.com>
2024-11-13 17:48:05 +0100

    Update dcel_animator.py
    added a few methods to get information from the actual dcel.
    updated most of the class to fix a bunch of bugs

--- notebooks/modules/data_structures/animation_objects/dcel_animator.py
+++ notebooks/modules/data_structures/animation_objects/dcel_animator.py
@@ -1,45 +1,82 @@
 from __future__ import annotations
-from base import AnimationObject, AnimationEvent, AppendEvent
-from ...data_structures import DoublyConnectedEdgeList, Vertex, HalfEdge
+from ...geometry.animation_base import AnimationObject, AnimationEvent, AppendEvent
+from ...data_structures import DoublyConnectedEdgeList, Vertex, HalfEdge, Face
 from ...geometry import Rectangle, Point, PointList
+from typing import Iterator
 
 class DCELAnimator(AnimationObject):
     def __init__(self, boundingBox : Rectangle):
+        super().__init__()
         self._dcel : DoublyConnectedEdgeList = DoublyConnectedEdgeList()
+        self._animation_events = []
+        self._illformed : bool = False
         self._init_bounding_box(boundingBox)
-        super.__init__()
+        print(self._dcel.edges)
     
     def _init_bounding_box(self, boundingBox: Rectangle):
-        upLeft = Point(boundingBox.left, boundingBox.upper)
-        lowLeft = Point(boundingBox.left, boundingBox.lower)
-        upRight = Point(boundingBox.left, boundingBox.lower)
-        lowRight = Point(boundingBox.right, boundingBox.lower)
+        #if the bounding box is illformed, all further calculations don't make sense
+        if((boundingBox.left == boundingBox.right) or (boundingBox.lower == boundingBox.upper)):
+            self._illformed = True
+            return
 
-        self.add_vertex(upLeft)
+        lowLeft = Point(boundingBox.left, boundingBox.lower)
+        upLeft = Point(boundingBox.left, boundingBox.upper)
+        upRight = Point(boundingBox.right, boundingBox.upper)
+        lowRight = Point(boundingBox.right, boundingBox.lower)
+        #add bottom left first so it can be accessed easily later
         self.add_vertex(lowLeft)
+        self.add_vertex(upLeft)
         self.add_vertex(upRight)
         self.add_vertex(lowRight)
+        self.add_edge(lowLeft, upLeft)
+        self.add_edge(upLeft, upRight)
+        self.add_edge(upRight, lowRight)
+        self.add_edge(lowRight, lowLeft)
 
-        self._dcel.add_edge(lowLeft, upLeft)
-        self._dcel.add_edge(upLeft, upRight)
-        self._dcel.add_edge(upRight, lowRight)
-        self._dcel.add_edge(lowRight, lowLeft)
+    def add_vertex(self, point : Point) -> Vertex:
+        v = self._dcel.add_vertex(point)
+        self._animation_events.append(AppendEvent(point))
+        return v
 
-    def add_vertex(self, point : PointList):
-        self._dcel.add_vertex(point)
-        self._animation_events.append(AppendEvent(PointList))
-
-    def add_edge(self, point1 : PointList, point2 : PointList):
-        self._dcel.add_edge(point1, point2)
+    def add_edge(self, point1 : Point, point2 : Point):
+        self._dcel.add_edge_by_points(point1, point2)
         self._animation_events.append(EdgeAddedEvent(point1, point2))
 
-    def add_vertex_on_edge(self, point : Point, edge : HalfEdge):
+    def add_vertex_on_edge(self, point : Point, edge : HalfEdge) -> Vertex:
         self._animation_events.append(EdgeRemovedEvent(edge.origin.point, edge.twin.origin.point))
-        v = self._dcel.add_vertex_in_edge(point, edge)
+        v = self._dcel.add_vertex_in_edge(edge, point)
         self._animation_events.append(AppendEvent(point))
 
         self._animation_events.append(EdgeRemovedEvent(edge.origin.point, edge.twin.origin.point))
         self._animation_events.append(EdgeRemovedEvent(edge.next.origin.point, edge.next.twin.origin.point))
+        return v
+
+    def points(self) -> Iterator[Point]:
+        points : list[PointList] = []
+        for v in self._dcel.vertices:
+            p = PointList(v.x, v.y)
+            data = []
+            for e in v.outgoing_edges():
+                data.append(Point(e.destination.x, e.destination.y))
+            p.data = data
+            points.append(p)
+        return iter(points)
+    
+    def get_next_face(self, v : Vertex, p : Point) -> Face:
+        return self._dcel.find_splitting_face(v,p)
+    
+    def get_bounding_box(self) -> list[HalfEdge]:
+        return self._dcel.outer_face.inner_half_edges()
+    
+    def get_outer_face(self) -> Face:
+        return self._dcel.outer_face
+    
+    def get_bottom_left(self) -> Vertex:
+        return self._dcel.start_vertex
+    
+    @property
+    def illformed(self):
+        return self._illformed
 
 
 class EdgeAddedEvent(AnimationEvent):
@@ -49,12 +86,11 @@ class EdgeAddedEvent(AnimationEvent):
 
     def execute_on(self, data : list[Point]):
         for p in data:
-            if not isinstance(p, PointList):
-                continue
-            if p is self._p1:
-                p.data().append(self._p2)
-            if p is self._p2:
-                p.data().append(self._p1)
+            if isinstance(p, PointList):
+                if p is self._p1:
+                    p.data.append(self._p2)
+                if p is self._p2:
+                    p.data.append(self._p1)
 
 class EdgeRemovedEvent():
     def __init__(self, p1: PointList, p2 : PointList):
@@ -66,7 +102,7 @@ class EdgeRemovedEvent():
             if not isinstance(p, PointList):
                 continue
             if p is self._p1 and p.data().__contains__(self._p2):
-                p.data().remove(self._p2)
+                p.data.remove(self._p2)
             if p is self._p2 and p.data().__contains__(self._p1):
-                p.data().remove(self._p1)
+                p.data.remove(self._p1)
 
