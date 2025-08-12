d73ad23307f3e5ea2ff5d2ef0932862a4d4ab001
DasWHYYYDerWoche <139765359+DasWHYYYDerWoche@users.noreply.github.com>
2025-08-12 12:38:18 +0200

    renamed objects to animation_objects

--- notebooks/modules/geometry/__init__.py
+++ notebooks/modules/geometry/__init__.py
@@ -1,4 +1,4 @@
 from .core import EPSILON, Orientation, VerticalOrientation, HorizontalOrientation, Point, PointReference, LineSegment, Rectangle
-from .objects import *
+from .animation_objects import *
 from .animation_base import *
 from .notebooks import *
---
+++ notebooks/modules/geometry/animation_objects.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+from collections import OrderedDict
+from typing import Any, Iterable, Iterator, Union
+
+from .core import *
+from .animation_base import AnimationEvent, AnimationObject, AppendEvent, PopEvent, ClearEvent, SetEvent, DeleteEvent, UpdateEvent, DeleteAtEvent
+
+
+class PointSequence(AnimationObject):
+    def __init__(self, points: Iterable[Point] = []):
+        self._points: list[Point] = []
+        self._animation_events: list[AnimationEvent] = []
+        for point in points:
+            self.append(point)
+
+    def points(self) -> Iterator[Point]:
+        return iter(self._points)
+
+    def animation_events(self) -> Iterator[AnimationEvent]:
+        return iter(self._animation_events)
+
+    def append(self, point: Point):
+        self._points.append(point)
+        self._animation_events.append(AppendEvent(point))
+
+    def pop(self) -> Point:
+        point = self._points.pop()
+        self._animation_events.append(PopEvent())
+        return point
+
+    def clear(self):
+        self._points.clear()
+        self._animation_events.append(ClearEvent())
+
+    def update(self, old : Point, new : Point):
+        i = self._points.index(old)
+        self._points.remove(old)
+        self._points.insert(i, new)
+        self._animation_events.append(UpdateEvent(old,new))
+
+    def delete(self, to_del : Point):
+        self._points.remove(to_del)
+        self._animation_events.append(DeleteEvent(to_del))
+
+    def animate(self, point: Point):
+        self._animation_events.append(AppendEvent(point))
+        self._animation_events.append(PopEvent())
+
+    def reset_animations(self):
+        self._animation_events = list(super().animation_events())
+
+    def _find(self, point: Point) -> Optional[Point]:
+        for i, seq_point in enumerate(self._points):
+            if point == seq_point:
+                return i
+        return None
+
+    def __repr__(self) -> str:
+        return self._points.__repr__()
+
+    def __add__(self, other: Any) -> PointSequence:
+        if not isinstance(other, PointSequence):
+            raise TypeError("Parameter 'other' needs to be of type 'PointSequence'.")
+        result = PointSequence()
+        result._points = self._points + other._points
+        result._animation_events = self._animation_events + other._animation_events
+        return result
+
+    def __len__(self) -> int:
+        return len(self._points)
+
+    def __getitem__(self, key: Any) -> Union[Point, PointSequence]:
+        if isinstance(key, Point):
+            key = self._find(key)
+            if key is None:
+                return None
+        if isinstance(key, int):
+            return self._points[key]
+        elif isinstance(key, slice) and (key.step is None or key.step == 1):
+            # This implementation is a hack, but it works for Graham Scan.
+            result = PointSequence()
+            result._points = self._points[key]
+            result._animation_events = self._animation_events[:]
+            return result
+        else:
+            raise ValueError("Parameter 'key' needs to be an integer or a slice with step 1.")
+
+    def __setitem__(self, key: Any, new_point: Point):
+        if isinstance(key, Point):
+            key = self._find(key)
+            if key is None:
+                return
+        elif not isinstance(key, int):
+            raise ValueError("Parameter 'key' needs to be an integer or a point")
+        self._points[key] = new_point
+        self._animation_events.append(SetEvent(key, new_point))
+
+    def __delitem__(self, key: Any):
+        print("deleting")
+        if not isinstance(key, int):
+            raise ValueError("Parameter 'key' needs to be an integer.")
+        del self._points[key]
+        self._animation_events.append(DeleteAtEvent(key))
+
+
+# TODO: Actually make this generic. For that, an Updater like for binary trees is needed. Maybe share type vars and aliases?
+class PointSequenceDict(AnimationObject):
+    def __init__(self):
+        self._intersections: OrderedDict[Point, set[LineSegment]] = OrderedDict()
+        self._animation_events: list[AnimationEvent] = []
+
+    def points(self) -> Iterator[Point]:
+        return iter(self._intersections)
+
+    def animation_events(self) -> Iterator[AnimationEvent]:
+        return iter(self._animation_events)
+
+    def add(self, intersection_point: Point, line_segments: Iterable[LineSegment]):
+        rounded_point = round(intersection_point, 5)
+        containing_segments: set[LineSegment] = self._intersections.setdefault(rounded_point, set())
+        if not containing_segments:
+            self._animation_events.append(AppendEvent(rounded_point))
+        containing_segments.update(line_segments)
+
+    def animate(self, point: Point):
+        self._animation_events.append(AppendEvent(point))
+        self._animation_events.append(PopEvent())
+
+    def __repr__(self) -> str:
+        return "\n".join(f"{point}: {segments}" for point, segments in self._intersections.items())
